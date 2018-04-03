2b6d7f22e5b2daa8bdc966988c615e87dbfcf28d
Jerry Padilla <jerrypadilla23@gmail.com>
2018-04-03 18:40:43 -0500

    Assignments

--- assignments/Express/survery/server.js
+++ assignments/Express/survery/server.js
@@ -28,7 +28,7 @@ app.get('/', function(req, res){
 
 app.post('/result', function(req, res){
      what_route(req.url);
-     
+
      req.session.postData = {
           name: req.body.name,
           location: req.body.location,
@@ -57,3 +57,92 @@ app.get('/destroy', function(req, res){
 app.listen(port, function(){
      console.log('Listening on port...',port);
 })
+
+
+
+
+
+
+
+
+
+The platform did this exercise in a way where they created a file scrictly for the routes and then required it in the server.js. I like this way because it keeps all the code simple and organized.
+
+
+
+
+// This will be what the server file looks like. Much cleaner.
+//
+// //load the express module
+// //load the path module
+// //load the body-parser
+// const express = require("express");
+// const path = require("path");
+// const bodyParser = require('body-parser');
+// // invoke const express and store the resulting application in const app
+// const app = express();
+// app.use(bodyParser.urlencoded());
+// app.use(express.static(path.join(__dirname, "./static")));
+// // setting up ejs and our views folder
+// app.set('views', path.join(__dirname, './views'));
+// app.set('view engine', 'ejs');
+//
+// //we're going to have /routes/index.js handle all of our routing
+// require('./routes/index.js')(app);
+// // setting server to run on port 3000
+// app.listen(3000, function() {
+//  console.log("listening on port 3000!");
+// });
+
+
+
+
+
+
+
+
+// This is the index.js in the route folder.
+
+// module.exports = function Route(app){
+// 	// root route to render the index.ejs view
+// 	app.get('/', function(req, res) {
+// 	 res.render("index");
+// 	});
+// 	// post route for adding a data from a survey
+// 	app.post('/result', function(req, res) {
+// 		const submittedInfo = {
+// 			name: req.body.name,
+// 			dojoLocation: req.body.dojoLocation,
+// 			favoriteLanguage: req.body.favoriteLanguage,
+// 			comment: req.body.comment
+//     };
+//
+// 	 	res.render("result", { userData: submittedInfo });
+// 	});
+// };
+
+
+
+
+// this is what the result html page looked like for this example.
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+// 	<meta charset="UTF-8">
+// 	<title>Survey Form</title>
+// </head>
+// <body>
+// 	<h2>Submitted Information</h2>
+// 	<ul>
+// 		<li>Name: <strong><%= userData.name %></strong></li>
+// 		<li>Dojo Location: <strong><%= userData.dojoLocation %></strong></li>
+// 		<li>Favorite Language: <strong><%= userData.favoriteLanguage %></strong></li>
+// 		<li>Comment: <strong><%= userData.comment %></strong></li>
+// 	</ul>
+// 	<a href="/"><h3>Back</h3></a>
+// </body>
+// </html>
+//
+//
+//
